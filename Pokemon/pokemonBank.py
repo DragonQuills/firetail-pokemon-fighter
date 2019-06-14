7dcb2291158a1647f08de450a5a261c3b57194c3
Kayden Drake <root@localhost.localdomain>
2019-06-13 22:42:27 -0600

    Added getAllPokemon method and tests for it

--- Pokemon/pokemonBank.py
+++ Pokemon/pokemonBank.py
@@ -1,8 +1,46 @@
 from pokemonClass import Pokemon
 
-class Bank:
-    def __init__(self):
+class PokeBank:
+    def __init__(self, fileName = "bankSaveFile.txt"):
         self.allPokemon = {}
+        self.saveFileName = fileName
+        #self.loadBank()
 
-    def addPokemon(self, name, species, stats):
+    def addPokemon(self, name, species, stats = [0, 0, 0, 0]):
         self.allPokemon[name] = Pokemon(name, species, stats)
+
+    def getAllPokemon(self):
+        pokeList = []
+        for name, pokemon in self.allPokemon.items():
+            pokeList.append(name + ": " + pokemon.species)
+        return pokeList
+#
+#     def saveBank(self):
+#         saveFile = None
+#         try:
+#             saveFile = open(self.saveFileName, 'r+')
+#         except:
+#             saveFile = open(self.saveFileName, 'w')
+#
+#         for name, pokemon in self.allPokemon.items():
+#             saveData = name + ", " + pokemon.species + ", " + str(pokemon.stats["hp"])+ ", " + str(pokemon.stats["atk"]) + ", " + str(pokemon.stats["def"]) + ", " + str(pokemon.stats["spd"]) + "\n"
+#             saveFile.write(saveData)
+#         saveFile.close()
+#
+#     def loadBank(self):
+#         try:
+#             loadFile = open(self.saveFileName, 'r')
+#         except:
+#             return
+#
+#         for line in loadFile:
+#             data = line.split(", ")
+#             name = data[0]
+#             species = data[1]
+#             stats = [data[2], data[3], data[4], data[5]]
+#             self.addPokemon(name, species, stats)
+#         loadFile.close()
+#
+# bank = PokeBank()
+# bank.addPokemon("Shell", "Dragonite")
+# bank.saveBank()
--- Pokemon/pokemonBankTest.py
+++ Pokemon/pokemonBankTest.py
@@ -1,8 +1,27 @@
-from pokemonBank import Bank
+from pokemonBank import PokeBank
 from pokemonClass import Pokemon
 
 def test_adds_pokemon():
-    bank = Bank()
+    bank = PokeBank()
     bank.addPokemon("Jazz", "Bellsprout", [15, 5, 5, 5])
     jazz = Pokemon("Jazz", "Bellsprout", [15, 5, 5, 5])
     assert bank.allPokemon["Jazz"].nickname == jazz.nickname
+
+def test_returns_names():
+    bank = PokeBank()
+    bank.addPokemon("Jazz", "Bellsprout", [15, 5, 5, 5])
+    bank.addPokemon("Shell", "Dragonite", [65, 22, 18, 35])
+    bank.addPokemon("Doggo", "Lillipup", [15, 5, 5, 5])
+    allMon = bank.getAllPokemon()
+    assert allMon[0] == "Jazz: Bellsprout"
+    assert allMon[1] == "Shell: Dragonite"
+    assert allMon[2] == "Doggo: Lillipup"
+
+
+
+
+
+# def test_save_and_load():
+#     file = open("testBankRead.txt", "w")
+#     file.write("Jackie Pumpkaboo 25 ")
+#     bank
