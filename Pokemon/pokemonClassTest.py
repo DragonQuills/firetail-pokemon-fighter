2987a4120bd2cda97d23f08dada7d08fe0e2fa1e
Kayden Drake <root@localhost.localdomain>
2019-06-13 18:33:37 -0600

    Add tests for the changeStat method

--- Pokemon/pokemonClassTest.py
+++ Pokemon/pokemonClassTest.py
@@ -15,5 +15,21 @@ def test_initializes_correctly():
     assert jazz.species == "Bellsprout"
     assert jazz.hitpoints == 0
 
-# def test_changes_stat():
-#     jazz = Pokemon()
+def test_changes_stat():
+    jazz = Pokemon("Jazz", "Bellsprout")
+    assert jazz.hitpoints == 0
+    assert jazz.attack == 0
+    assert jazz.defense == 0
+    assert jazz.speed == 0
+
+    jazz.changeStat("hp", 25)
+    assert jazz.hitpoints == 25
+
+    jazz.changeStat("atk", 5)
+    assert jazz.attack == 5
+
+    jazz.changeStat("def", 7)
+    assert jazz.defense == 7
+
+    jazz.changeStat("spd", 6)
+    assert jazz.speed == 6
