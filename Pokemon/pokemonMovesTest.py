115d0d6d3026af57aec5cb8c6f337317091332b4
Kayden Drake <root@localhost.localdomain>
2019-06-19 16:24:27 -0600

    Changes Move class so it don't throw an error for blank or invalid names

--- Pokemon/pokemonMovesTest.py
+++ Pokemon/pokemonMovesTest.py
@@ -12,3 +12,15 @@ def test_initializes_correctly():
     assert ember.type == "fire"
     assert ember.power == 40
     assert ember.accuracy == 100
+
+    blank = Move("")
+    assert blank.name == ""
+    assert blank.type == ""
+    assert blank.power == -1
+    assert blank.accuracy == -1
+
+    fake = Move("fake")
+    assert fake.name == "fake"
+    assert fake.type == ""
+    assert fake.power == -1
+    assert fake.accuracy == -1
