f87f01ca4ce085f042d58dc37311194f1b372475
Kayden Drake <root@localhost.localdomain>
2019-07-19 12:29:05 -0600

    Makes move class validate blank and invalid move names

--- Pokemon/moves.py
+++ Pokemon/moves.py
@@ -3,18 +3,21 @@ import pokebase as pb
 class Move:
     def __init__(self, moveName):
         cleanName = moveName.lower().replace(" ", "-")
-        try:
-            moveData = pb.move(cleanName)
-        except:
-            self.name = cleanName.strip()
-            self.type = ""
-            self.power = -1
-            self.accuracy = -1
-            return
-        self.name = moveData.names[2].name.strip()
-        self.type = moveData.type.name
-        self.power = moveData.power
-        self.accuracy = moveData.accuracy
+        if cleanName == "":
+            self.name = ""
+            self.type = "normal"
+            self.power = 0
+            self.accuracy = 0
+        else:
+            moveData = None
+            try:
+                moveData = pb.move(cleanName)
+            except:
+                moveData = pb.move("struggle")
+            self.name = moveData.names[2].name.strip()
+            self.type = moveData.type.name
+            self.power = moveData.power
+            self.accuracy = moveData.accuracy
 
     def __eq__(self, otherMove):
         if self.name == otherMove.name and self.type == otherMove.type and self.power == otherMove.power and self.accuracy == otherMove.accuracy:
