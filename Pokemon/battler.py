74b4725b5c0640ee4c02f498f792265273019135
Kayden Drake <root@localhost.localdomain>
2019-08-06 20:58:22 -0600

    Adds battler ability to check the type modifier of a move on a pokemon

--- Pokemon/battler.py
+++ Pokemon/battler.py
@@ -1,7 +1,36 @@
-import pokebase as poke
+import pokebase
 from pokemonClass import Pokemon
 
 class PokemonBattler:
-    def __init__(self, pokemon1, pokemon2):
-        self.pokemon1 = pokemon1
-        self.pokemon2 = pokemon2
+    def __init__(self):
+        pass
+
+    def moveTypeModifier(self, move, pokemon):
+        pokeTypes = pokemon.types
+        moveType = pokebase.type_(move.type)
+        modifier = 1
+
+        #create lists to store what the moves effectiveness is against certain types
+        doubleD = []
+        halfD = []
+        noD = []
+        for i in moveType.damage_relations.double_damage_to:
+            doubleD.append(i["name"])
+        for i in moveType.damage_relations.half_damage_to:
+            halfD.append(i["name"])
+        for i in moveType.damage_relations.no_damage_to:
+            noD.append(i["name"])
+
+        #for each type the pokemon has
+        for type in pokeTypes:
+            if type == "":
+                continue
+            #change the modifier if it's in any of the lists
+            if type in doubleD:
+                modifier = modifier*2
+            elif type in halfD:
+                modifier = modifier*0.5
+            elif type in noD:
+                modifier = modifier*0
+
+        return modifier
