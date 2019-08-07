4d2d3d440d06bb1eb557c525b61d068fef469a18
Kayden Drake <root@localhost.localdomain>
2019-08-06 20:56:25 -0600

    Fixes spelling and okemon type errors in test

--- Pokemon/test_battler.py
+++ Pokemon/test_battler.py
@@ -12,20 +12,20 @@ def test_move_effectiveness_function():
 
     #double damage test with two moves
     iceBeam = Move("ice beam")
-    pidgy = Pokemon("Pipi", "Pidgey", ["normal", "flying"])
+    pidgey = Pokemon("Pipi", "Pidgey", ["normal", "flying"])
     assert battler.moveTypeModifier(iceBeam, pidgey) == 2
 
     #no damage test
-    thunderBolt = Move("thunder bolt")
-    phanpy = ("Cheerio", "Phanpy", ["ground", ""])
+    thunderBolt = Move("thunderbolt")
+    phanpy = Pokemon("Cheerio", "Phanpy", ["ground", ""])
     assert battler.moveTypeModifier(thunderBolt, phanpy) == 0
 
     #quadrouble damage test
     rockBlast = Move("rock blast")
-    butterfree = ("Flutter", "Butterfree", ["bug", "flying"])
+    butterfree = Pokemon("Flutter", "Butterfree", ["bug", "flying"])
     assert battler.moveTypeModifier(rockBlast, butterfree) == 4
 
     #quarter damage test
     #reuses ember move from above
-    swampy = ("Swampy", "Swampert", ["water", "ground"])
-    assert battler.moveTypeModifier(ember, swampy) == 0.25
+    kingdra = Pokemon("King", "Kingdra", ["water", "dragon"])
+    assert battler.moveTypeModifier(ember, kingdra) == 0.25
